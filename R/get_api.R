55dff43fa7fc089fd0566831c686dec73028c6c1
baptiste-antoine <baptiste.antoine2001@gmail.com>
2024-03-05 21:24:33 +0100

    New methode

---
+++ R/get_api.R
@@ -0,0 +1,37 @@
+#chargement des librairies
+library(rjson)
+library(ggplot2)
+library(httr)
+
+#Récupération des informations
+url_service='https://api.geosas.fr/edr/collections/safran-isba/'
+data_info = fromJSON(file=url_service)
+
+for (parametre in data_info$parameter_names) {
+  name=names(parametre)
+  print(name)
+  print(parametre[[name]]$description)
+}
+
+param_name='T_Q'
+# Pour coord nécessite de remplacer l'espace par %20  
+# sinon la librairie rjson plante
+#'POINT(349598 6798263)' devient :
+coord='POINT(349598%206798263)' 
+projection='EPSG:2154'
+formatage='CoverageJSON'
+date='2022-07-01/2022-07-31'
+requete=sprintf('%sposition?coords=%s&crs=%s&parameter-name=%s&f=%s&datetime=%s',
+                url_service,coord,projection,param_name,formatage,date)
+print(requete)
+
+data = fromJSON(file=requete)
+
+date_value <- data$domain$axes$t$values
+values <- data$ranges[[param_name]]$values
+
+cat("Exemple de date pour définir le format:", date_value[1], "\n")
+df=data.frame(date=date_value,parametre=values)
+df$date <- as.POSIXct(df$date , format='%Y-%m-%dT%H-%M-%SZ')
+colnames(df)[2] <- param_name
+print(df)
